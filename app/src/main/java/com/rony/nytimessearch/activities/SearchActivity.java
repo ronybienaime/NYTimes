ded9f40fd5dc0ce3fa14c35dc00633c65413027d
Rony BIEN AIME <ronybienaime@gmail.com>
2018-02-18 17:59:01 -0500

    Initial commit

---
+++ app/src/main/java/com/rony/nytimessearch/activities/SearchActivity.java
@@ -0,0 +1,119 @@
+package com.rony.nytimessearch.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.rony.nytimessearch.Article;
+import com.rony.nytimessearch.ArticleArrayAdapter;
+import com.rony.nytimessearch.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class SearchActivity extends AppCompatActivity {
+
+
+    EditText etQuery;
+    GridView gvResults;
+    Button btnSearch;
+
+    ArrayList<Article> articles;
+    ArticleArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setupViews();
+
+    }
+     public void setupViews(){
+         etQuery=(EditText) findViewById(R.id.etQuery);
+         gvResults=(GridView) findViewById(R.id.gvResults);
+         btnSearch=(Button) findViewById(R.id.btnSearch);
+         articles=new ArrayList<>();
+         adapter=new ArticleArrayAdapter(this, articles);
+         gvResults.setAdapter(adapter);
+         gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+             @Override
+             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+             Intent i=new Intent(getApplicationContext(), ArticleActivity.class);
+             Article article=articles.get(position);
+             i.putExtra("article", article);
+             startActivity(i);
+             }
+         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onArticleSearch(View view) {
+         String query=etQuery.getText().toString();
+
+         //Toast.makeText(this, "Searching for "+ query, Toast.LENGTH_LONG).show();
+        AsyncHttpClient client=new AsyncHttpClient();
+        String url="http://api.nytimes.com/svc/search/v2/articlesearch.json";
+        RequestParams params=new RequestParams();
+
+        params.put("api-key","cc8eaad97de24dd588d0750e1a8634be");
+        //params.put("page",0);
+        //params.put("q", query);
+
+        client.get(url, params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("DEBUG", response.toString());
+                JSONArray articleJsonResults = null;
+
+                try {
+                    articleJsonResults = response.getJSONObject("response").getJSONArray("docs");
+                    adapter.addAll(Article.fromJSONArray(articleJsonResults));
+                  //adapter.notifyDataSetChanged();
+                    Log.d("DEBUG", articles.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+         });
+     }
+}
